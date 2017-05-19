2f4e384750f905f831a041bfceaad88fb4a33f39
Patrick Double <pat@patdouble.com>
2017-05-18 22:31:36 -0500

    Fix package install exec command

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,7 +6,7 @@ define sdkman::package(
 ) {
   require sdkman::install
 
-  $sdkman_init = "source ${::boxen_user}/.sdkman/bin/sdkman-init.sh"
+  $sdkman_init = "source /Users/${::boxen_user}/.sdkman/bin/sdkman-init.sh"
 
   exec { "install-${package}-${version}":
     command => "su -c '${sdkman_init} && sdk install ${package} ${version}' - ${::boxen_user}",
