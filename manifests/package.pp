feaaa73064c1d1a41a0260408b0f508fd9e5fce0
Patrick Double <pat@patdouble.com>
2017-05-19 07:06:39 -0500

    Do install using bash

--- manifests/package.pp
+++ manifests/package.pp
@@ -9,13 +9,13 @@ define sdkman::package(
   $sdkman_init = "source /Users/${::boxen_user}/.sdkman/bin/sdkman-init.sh"
 
   exec { "install-${package}-${version}":
-    command => "su -c '${sdkman_init} && sdk install ${package} ${version}' - ${::boxen_user}",
+    command => "bash -c '${sdkman_init} && sdk install ${package} ${version}'",
     creates => "/Users/${::boxen_user}/.sdkman/candidates/${package}/${version}",
   }
 
   if($default) {
     exec { "set-${package}-default":
-      command => "${sdkman_init} && sdk default ${package} ${version}",
+      command => "bash -c '${sdkman_init} && sdk default ${package} ${version}'",
       require => Exec["install-${package}-${version}"],
     }
   }
